dd56323c99ce073253f7e28acdcede018078c010
jaklahaulps <liu.perry@gmail.com>
2018-12-18 14:18:49 -0500

    formatting for output fixed

--- test_script.js
+++ test_script.js
@@ -1,6 +1,7 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
 const input = process.argv[2];
+const moment = require("moment");
 
 const client = new pg.Client({
   user     : settings.user,
@@ -18,16 +19,19 @@ client.connect((err) => {
     return console.error("Connection Error", err);
   }
 
-  function printResult(err, result){ 
-    console.log(result.rows); //output: 1
+  function printResult(err, results) { 
+    console.log(`Found ${results.rows.length} person(s) by the name ${input}:`)
+    results.rows.forEach (function (result, index) {
+      console.log(`- ${index+1}: ${result.first_name} ${result.last_name}, born '${moment(result.birthdate).format('YYYY-MM-DD')}'`)
+    });
   };
 
-  client.query(`SELECT * FROM famous_people WHERE first_name = $1::text OR last_name = $1::text`,[input], (err, result) => {
+  client.query(`SELECT * FROM famous_people WHERE first_name = $1::text OR last_name = $1::text`,[input], (err, results) => {
       console.log("Searchng ...");
       if (err) {
         return console.error("error running query", err);
       } else {
-        printResult(null, result);
+        printResult(null, results);
       }
       client.end();
     })
